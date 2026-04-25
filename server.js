a0e884cb3c900c2c022812c748c6bd1f7b7ff42e
maherhato1986 <ADMIN@RK-LIFTS.COM>
2026-04-25 19:45:49 +0300

    Remove duplicate path import
    Removed duplicate 'path' import from server.js.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const axios = require('axios');
 const path = require('path');
 const crypto = require('crypto');
-const path = require('path');
+
 
 const app = express();
 
